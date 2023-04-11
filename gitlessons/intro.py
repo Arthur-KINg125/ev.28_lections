fcbd9adca33bd0c621718e15f625be24f685a2f6
Arthur-KINg125 <artagg126@gmail.com>
2023-04-11 20:47:19 +0600

    added git instructions

--- gitlessons/intro.py
+++ gitlessons/intro.py
@@ -17,3 +17,11 @@
 
 # 5. git push <origin> <main> - команда отправки всех ваших изменения на удаленный репозиторий.
 
+# -----------------------------------------
+
+# 1) git init
+    # 2) git add
+    # 3) git commit -m ''
+# 4) git remote add origin <url>
+    # 5) git push origin main
+
